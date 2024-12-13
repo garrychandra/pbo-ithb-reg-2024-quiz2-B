a4d652f4586bb08e4388e32a1086e8dfecc8a3d4
gachandr <gachandr@bu.edu>
2024-12-13 14:19:22 +0700

    testpackage

---
+++ java/main.java
@@ -0,0 +1,10 @@
+import model.Users;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Users abc = new Users();
+        abc.setEmail("abc");
+        System.out.println(abc.getEmail());
+    }
+}
---
+++ java/model/Artworks.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Artworks {
+    private int id;
+    private String title;
+    private String descriptio;
+    private String image_path;
+    private int user_id;
+
+}
